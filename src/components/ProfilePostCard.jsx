556ece65cb29f48ba73945f5159bef4f1f8ddb60
AmirRosli <4mirosli@gmail.com>
2024-08-03 17:52:35 +0800

    initial commit

---
+++ src/components/ProfilePostCard.jsx
@@ -0,0 +1,43 @@
+import { Button, Col, Image, Row } from "react-bootstrap";
+
+
+export default function ProfilePostCard({ content }) {
+    const pic = "";
+
+    return (
+        <Row
+            className="p-3"
+            style={{
+                borderTop: "1px solid #D3D3D3",
+                borderBottom: "1px solid #D3D3D3"
+            }}
+        >
+            <Col sm={1}>
+                <Image src={pic} fluid roundedCircle />
+            </Col>
+
+            <Col>
+                <strong>Amir</strong>
+                <span> @amir.rosli * Apr 1</span>
+                <p>{content}</p>
+                <div className="d-flex justify-content-between">
+                    <Button variant="light">
+                        <i className="bi bi-chat"></i>
+                    </Button>
+                    <Button variant="light">
+                        <i className="bi bi-repeat"></i>
+                    </Button>
+                    <Button variant="light">
+                        <i className="bi bi-heart"></i>
+                    </Button>
+                    <Button variant="light">
+                        <i className="bi bi-graph-up"></i>
+                    </Button>
+                    <Button variant="light">
+                        <i className="bi bi-upload"></i>
+                    </Button>
+                </div>
+            </Col>
+        </Row>
+    )
+}
